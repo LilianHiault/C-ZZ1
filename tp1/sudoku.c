0c25b7d867a71986a56b2d650982429b3b46e7ca
Lilian Hiault <lilian.hiault@laposte.net>
2020-09-23 18:08:16 +0200

    Updated sudoku.c : initialiser()

--- tp1/sudoku.c
+++ tp1/sudoku.c
@@ -1,7 +1,87 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
 
-int main(int argc, char * argv[])
+#define N 9
+
+int initialiser(int grille[][N])
 {
+  int i, j;
+  for(i=0; i<N; i++)
+    {
+      for(j=0; j<N; j++)
+	{
+	  grille[i][j] = 0;
+	}
+    }
+  return 0;
+}
+
+void afficher(int grille[][N])
+{
+  int i, j;
+  int racine = sqrt(N);
+  for(i=0; i<N; i++)
+    {
+      if(i % racine == 0)
+	{
+	  printf("-------------------------\n");
+	}
+      for(j=0; j<N; j++)
+	{
+	  if(j % racine == 0)
+	    {
+	      printf("| ");
+	    }
+	  printf("%d ", grille[i][j]);
+	}
+      printf("|\n");
+    }
+  printf("-------------------------\n");
+}
+
+int generer(int grille[][N])
+{
+  int i, j, k;
+  int nonNuls;
+  int affiche;
+
+  for(j=0;j<N; ++j) 
+    {
+      for(i=0; i<N; ++i)
+	grille[j][i] = (i + j*3 +j /3) %N +1 ; 
+    }
+
+  for(i=0; i<N; i++)
+    {
+      for(j=0; j<N; j++)
+	{
+	  affiche = rand() % 2;
+	  if (!affiche)
+	    {
+	      grille[i][j] = 0;
+	      nonNuls++;
+	    }
+	}
+    }
+  return nonNuls;
+}
+
+int main()
+{
+  int grille [N][N];
+
+  int remplissage = N * N;
+
+  srand(time(NULL));
+
+  printf("SUDOKU !\n");
+  
+  initialiser(grille);
+  remplissage = generer(grille);
+  afficher(grille);
+
   return 0;
 }
     
