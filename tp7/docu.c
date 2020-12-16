21aef0e588a01232d0c7bdaf030b318237c5a1f4
Lilian Hiault <lilian.hiault@laposte.net>
2020-12-16 17:40:56 +0100

    Update docu.c: working list

--- tp7/docu.c
+++ tp7/docu.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 
@@ -19,36 +20,91 @@ typedef struct liste
 void afficher(liste_t li)
 {
   cellule_t * cour = li.tete;
+  printf("=> Début\n");
   while(cour != NULL)
     {
       printf("%s\n", cour->ligne);
       cour = cour->suiv;
     }
+  printf("=> Fin\n");
 }      
 
 
 void inser_debut(liste_t * li, char chaine[255])
 {
-  cellule_t element;
-  strcpy(element.ligne, chaine);
-  element.suiv = li->tete;
-  li->tete = &element;
-  if (li->fin == NULL)
+  cellule_t * element = (cellule_t*)malloc(sizeof(cellule_t));
+  if (element != NULL)
     {
-      li->fin = &element;
+      strcpy(element->ligne, chaine);
+      element->suiv = li->tete;
+      
+      li->tete = element;
+      if (li->fin == NULL)
+	{
+	  li->fin = element;
+	}
     }
   /* afficher(*li); */
 }
   
 
-void ajout_fin(liste_t li);
+void ajout_fin(liste_t * li, char chaine[255])
+{
+  cellule_t * element = (cellule_t*)malloc(sizeof(cellule_t));
+  if (element != NULL)
+    {
+      // Fin de la liste
+      strcpy(element->ligne, chaine); // Remplit les champs de élément      
+      element->suiv = NULL;
+
+      li->fin->suiv = element;
+      li->fin = element;
+      if (li->tete == NULL)
+	{
+	  li->tete = element;
+	}
+    }
+}
+
+void liberer(liste_t * li)
+{
+  cellule_t * cour = li->tete;
+  cellule_t * lib = cour;
+  if(li->tete != NULL)
+    {
+      while(cour != NULL)
+	{
+	  lib = cour;
+	  cour = cour->suiv;
+	  free(lib);
+	}
+      li->tete = NULL;
+      li->fin = NULL;
+    }
+}      
+  
 
 int main()
 {
   char chaine[255] = "Hello world!";
   liste_t li = {NULL, NULL};
+
   /* afficher(li); */
-  inser_debut(&li, chaine);
+  /* inser_debut(&li, chaine); */
+  /* strcpy(chaine, "Coucou"); */
+  /* inser_debut(&li, chaine); */
+  /* strcpy(chaine, "Fini"); */
+  /* ajout_fin(&li, chaine); */
+  /* afficher(li); */
+
+  while (!feof(stdin))
+    {
+      printf("Entrez une phrase (C-d pour quitter) :\n");
+      fgets(chaine, 255, stdin);
+      printf("%s\n", chaine);
+    }
+
+  liberer(&li);
   afficher(li);
   return 0;
 }
